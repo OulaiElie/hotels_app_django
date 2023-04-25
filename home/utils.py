d45316d58bb3b57f5d3f9f90b24c9c80e2b27aa4
JOB'S <eliyanessemon@gmail.com>
2023-04-25 12:23:53 +0200

    app django hotels

---
+++ home/utils.py
@@ -0,0 +1,8 @@
+from datetime import date
+from .models import Hotel
+
+hotel1 = Hotel(nom="Hotel Villa", date_ouverture=date(2023, 5, 1), date_fermeture=date(2023, 10, 31))
+hotel1.save()
+
+hotel2 = Hotel(nom="Hotel room top", date_ouverture=date(2023, 6, 1), date_fermeture=date(2023, 9, 30))
+hotel2.save()
